bc0beecc5f66fe75f2ad5cdfb153757c32c6c787
sunnaosp <sunnaosp@hotmail.com>
2022-03-12 12:48:38 +0000

    initial commit with template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Typescript Tailwind CSS started</h1>
+      <h2>Starter for TailwindCSS with React & Typescript</h2>
+    </div>
+  );
+}
